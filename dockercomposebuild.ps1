84ed40912f7e1adf43c99c5bfb036e732db5c00b
art.hontar <art.hontar@andersenlab.com>
2020-05-10 22:16:01 +0300

    add scripts

---
+++ dockercomposebuild.ps1
@@ -0,0 +1,18 @@
+cd ..
+docker build -t identityserver ./IdentityServer
+docker build -t monitoring.api ./Monitoring
+docker build -t quiz.api ./Quiz
+docker build -t statistic.api ./Statistic
+docker build -t user.api ./User
+
+docker tag identityserver:latest 013763960236.dkr.ecr.us-east-1.amazonaws.com/identityserver:latest
+docker tag monitoring.api:latest 013763960236.dkr.ecr.us-east-1.amazonaws.com/monitoringapi:latest
+docker tag quiz.api:latest 013763960236.dkr.ecr.us-east-1.amazonaws.com/appquiz:latest
+docker tag statistic.api:latest 013763960236.dkr.ecr.us-east-1.amazonaws.com/statisticapi:latest
+docker tag user.api:latest 013763960236.dkr.ecr.us-east-1.amazonaws.com/userapi:latest
+
+docker push 013763960236.dkr.ecr.us-east-1.amazonaws.com/identityserver:latest
+docker push 013763960236.dkr.ecr.us-east-1.amazonaws.com/monitoringapi:latest
+docker push 013763960236.dkr.ecr.us-east-1.amazonaws.com/appquiz:latest
+docker push 013763960236.dkr.ecr.us-east-1.amazonaws.com/statisticapi:latest
+docker push 013763960236.dkr.ecr.us-east-1.amazonaws.com/userapi:latest
--- nugetpack.ps1
+++ nugetpack.ps1
@@ -3,3 +3,5 @@ dotnet pack Shared.sln -p:PackageVersion=1.1.0 -c Release -o ../pack
 
 cd ../pack
 dotnet nuget push *.nupkg -k oy2b6pgab6neovprl3ia3pt7fmz7aooelik4judyqoyj7a -s https://api.nuget.org/v3/index.json
+
+rmdir pack
